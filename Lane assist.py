03220dca851d1bce341f26ff89a5513fbac7f452
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2018-07-16 20:22:56 +0100

    Web cam read and color change

--- Lane assist.py
+++ Lane assist.py
@@ -1,1 +1,31 @@
-#testing
+import cv2
+
+'#Create an object. zero for external camera'
+video = cv2.VideoCapture(0)
+a = 0
+
+
+while True:
+    a = a+1
+    '#Create a frame object'
+    check, frame = video.read()
+
+    #print(check)
+    #print(frame)
+
+    '#convert to grey scale'
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    '#show the frame'
+    cv2.imshow("Webcam1", gray)
+    cv2.imshow("Webcam2", frame)
+
+    '#Close when any key pressed(milliseconds)'
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+
+cv2.destroyAllWindows()
