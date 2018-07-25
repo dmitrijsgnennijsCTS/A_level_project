44b37b0579b6e6821a2fcd4bdf0651eafae7de0b
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2018-07-25 10:21:20 +0100

    Cleaning up and setting the layout

--- Lane assist.py
+++ Lane assist.py
@@ -1,31 +1,26 @@
+# Imported libraries required for functions to operate efficiently
 import cv2
-import numpy as np
 
-'#Create an object. zero for external camera'
-video = cv2.VideoCapture("test video.mp4")
+# Global variables
+# Getting the video input
+video_input = cv2.VideoCapture("test video.mp4")
 
 
+# Functions required for the process of lane detection
+def grayscale(frame):
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Applies the grayscale to the original image
+
+
+# Main code
 while True:
-    '#Create a frame object'
-    check, frame = video.read()
-    ROI = frame[470:635, 350:900] #region of interest
-    cv2.imshow("Region of interest", ROI)
-    hsv = cv2.cvtColor(ROI, cv2.COLOR_BGR2HSV)
-    #hue sat value
-    lower_yellow = np.array([0, 0, 100])
-    upper_yellow = np.array([180, 130, 255])
+    check, frame = video_input.read()
+    cv2.imshow("Grayscale", grayscale(frame))
 
-    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    res = cv2.bitwise_and(ROI, ROI, mask=mask)
-
-    blur = cv2.medianBlur(res, 3)
-
-    cv2.imshow("blur3", blur)
-    '#Close when any key pressed(milliseconds)'
     key = cv2.waitKey(25)
 
     if key == ord('q'):
         break
 
-video.release()
+video_input.release()
 cv2.destroyAllWindows()
